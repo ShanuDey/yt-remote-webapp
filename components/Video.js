4a64e208354427a371faabd72b928865699570b4
Shanu Dey <sd4shanudey@gmail.com>
2022-03-24 19:04:55 +0530

    added Video Component for each video
    Signed-off-by: Shanu Dey <sd4shanudey@gmail.com>

---
+++ components/Video.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Card, Button } from 'react-bootstrap';
+import { sendPostRequest } from './Search';
+
+export const Video = ({ video }) => {
+  const views = String(video.views).padStart(10, ' ');
+
+  const handleVideoPlay = async () => {
+    const data = `video_url=${encodeURIComponent(video.url)}`;
+
+    // API endpoint where we send form data.
+    const endpoint = 'http://localhost:8080/start_video';
+
+    // Form the request for sending data to the server.
+    const options = {
+      // The method is POST because we are sending data.
+      method: 'POST',
+      // Tell the server we're sending JSON.
+      headers: {
+        // 'Content-Type': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+        'Access-Control-Allow-Origin': '*',
+      },
+      // Body of the request is the JSON data we created above.
+      body: data,
+    };
+
+    // Send the form data to our forms API on Vercel and get a response.
+    const response = await fetch(endpoint, options);
+
+    // Get the response data from server as JSON.
+    // If server returns the name submitted, that means the form works.
+    const result = await response.json();
+    console.log(result);
+  };
+
+  return (
+    <Card border='primary'>
+      <Card.Img variant='top' src={video.thumbnail} />
+      <Card.Body>
+        <Card.Title>{video.title}</Card.Title>
+        <Card.Text>{video.description.slice(0, 50)}...</Card.Text>
+        <div className='text-end'>
+          <Button variant='success' onClick={handleVideoPlay}>
+            Play
+          </Button>
+        </div>
+      </Card.Body>
+      <Card.Footer className='text-muted text-center'>
+        {video.duration.timestamp}
+      </Card.Footer>
+    </Card>
+  );
+};
