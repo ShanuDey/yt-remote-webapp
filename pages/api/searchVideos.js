4b7d506ac0bbb44075675fb54cf0c303af92047b
Shanu Dey <sd4shanudey@gmail.com>
2022-03-24 19:02:12 +0530

    videoSearch api only handle post requests and sends 8 video as response
    Signed-off-by: Shanu Dey <sd4shanudey@gmail.com>

--- pages/api/searchVideos.js
+++ pages/api/searchVideos.js
@@ -1,7 +1,14 @@
 import yts from 'yt-search';
 
 export default async function handler(req, res) {
-  const searchResult = await yts(req.query.searchKeyword);
-  const videos = searchResult.videos.slice(0, 3);
-  res.status(200).json(videos);
+  if (req.method === 'POST') {
+    // Process a POST request
+    console.log('query', req.body);
+    const searchResult = await yts(req.body.searchKeyword);
+    const videos = searchResult.videos.slice(0, 8);
+    res.status(200).json(videos);
+  } else {
+    // Handle any other HTTP method
+    res.status(200).json({ error: 'something went wrong' });
+  }
 }
