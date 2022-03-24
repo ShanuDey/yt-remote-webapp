d6793d03ca9e13ed2c81fffbf33e88b541d286f7
Shanu Dey <sd4shanudey@gmail.com>
2022-03-24 20:01:04 +0530

    fixed home link
    Signed-off-by: Shanu Dey <sd4shanudey@gmail.com>

--- components/Header.js
+++ components/Header.js
@@ -14,11 +14,11 @@ export const Header = () => {
   return (
     <Navbar bg='light' expand='lg'>
       <Container>
-        <Navbar.Brand href='#home'>YT Remote</Navbar.Brand>
+        <Navbar.Brand href='/'>YT Remote</Navbar.Brand>
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav'>
           <Nav className='me-auto'>
-            <Nav.Link href='#home'>Home</Nav.Link>
+            <Nav.Link href='/'>Home</Nav.Link>
             <Nav.Link href='#link'>Remote Setup</Nav.Link>
             <NavDropdown title='Settings' id='basic-nav-dropdown'>
               <NavDropdown.Item
