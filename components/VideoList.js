a1355985b4823ded4ab00cc89fd9b13011e780c3
Shanu Dey <sd4shanudey@gmail.com>
2022-03-26 12:34:02 +0530

    added bottom margin for video list
    Signed-off-by: Shanu Dey <sd4shanudey@gmail.com>

--- components/VideoList.js
+++ components/VideoList.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Pagination } from 'react-bootstrap';
 import { VideosContext } from '../contexts/VideosContext';
 import { Video } from './Video';
 
@@ -7,7 +7,7 @@ export const VideoList = () => {
   const videosContext = useContext(VideosContext);
 
   return (
-    <div className='container mt-5'>
+    <div className='container mt-5 mb-5'>
       <Row xs={1} sm={2} md={4} className='g-4'>
         {videosContext.videos.map((video) => (
           <Col key={video.videoId}>
