3a3e34dffb786ff074845285c45cb03214e82b94
Shanu Dey <sd4shanudey@gmail.com>
2022-03-26 12:13:35 +0530

    removed unnecessary redirecting to home added toast msg for reboot and shutdown
    Signed-off-by: Shanu Dey <sd4shanudey@gmail.com>

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import { toast } from 'react-toastify';
 import { RemoteContext } from '../contexts/RemoteContext';
 
 export const Header = () => {
@@ -8,9 +9,11 @@ export const Header = () => {
   const handleClick = async (e, path) => {
     if (path === '/reboot') {
       await fetch(`${remoteContext.remoteHost}/reboot`);
+      toast.error('Rebooting...');
     }
     if (path === '/shutdown') {
       await fetch(`${remoteContext.remoteHost}/shutdown`);
+      toast.error('Shutting Down...');
     }
   };
 
@@ -24,17 +27,11 @@ export const Header = () => {
             <Nav.Link href='/'>Home</Nav.Link>
             <Nav.Link href='/views/remote_setup'>Remote Setup</Nav.Link>
             <NavDropdown title='Settings' id='basic-nav-dropdown'>
-              <NavDropdown.Item
-                href='/'
-                onClick={(e) => handleClick(e, '/reboot')}
-              >
+              <NavDropdown.Item onClick={(e) => handleClick(e, '/reboot')}>
                 Reboot
               </NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item
-                href='/'
-                onClick={(e) => handleClick(e, '/shutdown')}
-              >
+              <NavDropdown.Item onClick={(e) => handleClick(e, '/shutdown')}>
                 Shutdown
               </NavDropdown.Item>
             </NavDropdown>
